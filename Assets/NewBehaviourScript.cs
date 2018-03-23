73958461995a9cb50f3453786bf0274e605536ad
Wu-JiaYing <s1050422@gm.pu.edu.tw>
2018-03-23 11:57:04 +0800

    init

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour {
 
     Rigidbody rb;
+
+    public Text countText;
+    public Text winText;
+
+    int count;
     public float speed;
 
 	// Use this for initialization
 	void Start () {
 		
         rb=GetComponent<Rigidbody>();
+
+        countText.text =" 0";
+        countText.text = "分數:";
+        winText.text = "";
 	}
 	
 	// Update is called once per frame
@@ -22,4 +32,17 @@ public class NewBehaviourScript : MonoBehaviour {
         rb.AddForce(new Vector3(x,0,z) * speed);
 
 	}
+
+    void OnTriggerEnter(Collider other) { 
+        
+           
+            other.gameObject.SetActive(false);
+            count++;
+            countText.text = "分數:" + count.ToString();
+
+        if(count>=2){
+            winText.text = "You Win!";
+        }
+            
+    }
 }
